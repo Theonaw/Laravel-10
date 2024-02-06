287cb2d88886e7bbac6d5406f78b38f9c0adb6c8
Theonaw <theonawro@gmail.com>
2024-02-06 15:30:37 +0100

    Ajout commentaire dans les reservations
    ajout du "if" dans reservationShow pour les commentaires

--- resources/views/reservationShow.blade.php
+++ resources/views/reservationShow.blade.php
@@ -9,6 +9,8 @@
         <h2>Heure: {{ $reservationShow->heure }}</h2>
         <h2>Jour: {{ $reservationShow->jour }}</h2>
         <h2>Téléphone: {{ $reservationShow->telephone}}</h2>
-        
+        @if ($reservationShow->commentaire)
+            <h2>Commentaire: {{ $reservationShow->commentaire}}</h2>
+        @endif
     </section>
 @endsection
