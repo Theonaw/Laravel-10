51c483613d06cfb6fcea8876010411858a5aa220
Firesun <quentincochez@hotmail.com>
2024-02-19 15:28:23 +0100

    ajout de mention legal , footer

--- resources/views/mention.blade.php
+++ resources/views/mention.blade.php
@@ -2,9 +2,23 @@
 
 @section('title', 'Mentions Legales')
 @section('content')
+<div class="mention">
 <h1>Mentions légales</h1>
 <br>
-<h3>identité :</h3> 
+<h3>Identité :</h3> 
 <p>entrepreneur : Théo Nawrocki , Quentin Cochez</p>
+<h3>Ocnamo</h3>
+<p>adresse : 5 Bd Louis XIV, 59800 Lille</p>
+<p>télephone : 01 23 45 67 89</p>
+<h3>Numéro d'immatriculation RCS :</h3>
+<p>RCS Lille 123 456 789</p>
+<h3>Numéro d'identification à la TVA</h3>
+<p>FR 32 123456789</p>
+<h2>Conditions d’utilisation</h2>
+<h3>Confort d’utilisation</h3>
+<p>Ce site est proposé en différents langages web (HTML, HTML5, Javascript, CSS, etc…)<br> pour un meilleur confort d’utilisation et un graphisme plus agréable.
+    <br>Nous vous recommandons de recourir à des navigateurs modernes comme Safari, Firefox, Google Chrome… </p>
+</div>
+
 
 @endsection
